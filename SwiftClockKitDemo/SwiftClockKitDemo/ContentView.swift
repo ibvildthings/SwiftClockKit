d419f6336e246a3a1ed6f52baf4160f9012be35a
mylo.xyloto <iampriteshdesai@gmail.com>
2025-05-15 05:18:25 -0700

    Added a demo project

---
+++ SwiftClockKitDemo/SwiftClockKitDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftClockKitDemo
+//
+//  Created by Pritesh Desai on 5/15/25.
+//
+
+import SwiftUI
+import SwiftClockKit
+
+struct ContentView: View {
+    var body: some View {
+        ClockView()
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftClockKitDemo/SwiftClockKitDemo/SwiftClockKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftClockKitDemoApp.swift
+//  SwiftClockKitDemo
+//
+//  Created by Pritesh Desai on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftClockKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
